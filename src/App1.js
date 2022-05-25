2541afee83ced78fa5a2923706b370f92653acee
tumblbugkevin <kevin@tumblbug.com>
2022-05-25 11:47:38 +0900

    [BC-1] 파일수정

--- src/App1.js
+++ src/App1.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App1.js</code> and save to reload.
+          Edit1 <code>src/App1.js</code> and save to reload.
         </p>
         <a
           className="App-link"
